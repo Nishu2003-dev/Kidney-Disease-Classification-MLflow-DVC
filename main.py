b3192737efb37b5ef879f2b7f15b1a3436eb981b
Nishu2003-dev <nishantgupta0945@gmail.com>
2026-06-08 20:36:30 +0530

    Ingestion Pipeline

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from src.Kidney_Disease_cnn_Classification import logger
+from Kidney_Disease_cnn_Classification import logger 
+from Kidney_Disease_cnn_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainPipeline
 
-logger.info("Welcome to the Kidney Disease Classification Project")
+
+STAGE_NAME="Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataIngestionTrainPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<< \n\nX=======X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+    
