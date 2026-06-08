bde01aab1cb53d37b0fa5f28b870217c84f177b0
Nishu2003-dev <nishantgupta0945@gmail.com>
2026-06-09 02:44:48 +0530

    Prepare base Model added

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from Kidney_Disease_cnn_Classification import logger 
 from Kidney_Disease_cnn_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainPipeline
+from Kidney_Disease_cnn_Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
-STAGE_NAME="Data Ingestion Stage"
+#STAGE_NAME="Data Ingestion Stage"
 
-try:
+'''try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<")
     obj = DataIngestionTrainPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<< \n\nX=======X")
 except Exception as e:
     logger.exception(e)
+    raise e '''
+
+
+STAGE_NAME = "Prepare base Model"
+
+try:
+    logger.info(f"************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n")
+        
+except Exception as e:
+    logger.exception(e)
     raise e
-
-
+        
     
--- src/Kidney_Disease_cnn_Classification/config/configuration.py
+++ src/Kidney_Disease_cnn_Classification/config/configuration.py
@@ -1,6 +1,7 @@
 from Kidney_Disease_cnn_Classification.constants import CoNFIG_FILE_PATH,PARAMS_FILE_PATH
 from Kidney_Disease_cnn_Classification.utils.common import read_yaml,create_directories
-from Kidney_Disease_cnn_Classification.entity.config_entity import Dataingestionconfig
+from Kidney_Disease_cnn_Classification.entity.config_entity import Dataingestionconfig,PrepareBaseModelConfig
+from pathlib import Path
 
 
 class ConfigurationManager:
@@ -24,3 +25,32 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config
+    
+class ConfigurationManager:
+    def __init__(
+        self,
+        config_filepath = CoNFIG_FILE_PATH,
+        param_filepath= PARAMS_FILE_PATH
+    ):
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(param_filepath)
+        
+        create_directories([self.config.artifacts_root])
+        
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+        
+        prepare_base_model_config= PrepareBaseModelConfig(
+            root_dir = Path(config.root_dir),
+            base_model_path = Path(config.base_model_path),
+            updated_base_model_path= Path(config.updated_base_model_path),
+            params_image_size= self.params.IMAGE_SIZE,
+            params_learning_rate = self.params.LEARNING_RATE,
+            params_include_top= self.params.INCLUDE_TOP,
+            params_weights = self.params.WEIGHTS,
+            params_classes = self.params.CLASSES
+        )
+        
+        return prepare_base_model_config
